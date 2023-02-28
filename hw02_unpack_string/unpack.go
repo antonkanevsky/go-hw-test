ca304734d50141a285f48e53db62508c44049727
Anton Kanevskiy <antonkanevsky89@gmail.com>
2023-03-01 03:41:12 +0500

    HW2 fixed errors in algorithm, added test case for russian symbols in string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,17 +11,21 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var res strings.Builder
-	for i := 0; i < len(s); i++ {
-		if unicode.IsDigit(rune(s[i])) {
+	sRunes := []rune(s)
+	for i := 0; i < len(sRunes); i++ {
+		if unicode.IsDigit(sRunes[i]) {
 			return "", ErrInvalidString
 		}
 
-		if i+1 < len(s) && unicode.IsDigit(rune(s[i+1])) {
-			repCnt, _ := strconv.Atoi(string(s[i+1]))
-			res.WriteString(strings.Repeat(string(s[i]), repCnt))
+		if i+1 < len(sRunes) && unicode.IsDigit(sRunes[i+1]) {
+			repCnt, err := strconv.Atoi(string(sRunes[i+1]))
+			if err != nil {
+				return "", err
+			}
+			res.WriteString(strings.Repeat(string(sRunes[i]), repCnt))
 			i++
 		} else {
-			res.WriteString(string(s[i]))
+			res.WriteString(string(sRunes[i]))
 		}
 	}
 
